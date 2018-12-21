2ec5194dcd3fb1477e9ce0834dcffc23da52c3ab
Antonis Karamitros <antouankar@gmail.com>
2018-12-21 11:05:22 +0200

    Create initial react app with redux

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <p>Hi</p>
+          </header>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
